86cacc083f703a0e935ea19ce7001d747a1d6898
向水玉 <191793234@qq.com>
2023-05-16 23:17:18 +0800

    use vscode commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hello-world/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     HelloWorld
   }
@@ -15,12 +14,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+ margin: 0;
+ padding: 0; 
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <p>HomeView</p>
+  </div>
+</template>
